d799ce3a4bb5af8054079fca9bc118269d9a2e30
paulwoojungkim <wj2kim@gmail.com>
2020-08-05 23:48:18 +0900

    commit init

---
+++ client/src/App.js
@@ -0,0 +1,46 @@
+import React, { useState, useState, useRef } from "react";
+import "./App.css";
+import io from "socket.io-client";
+import Peer from "simple-peer";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Row = styled.div`
+  display: flex;
+  width: 100%;
+`;
+
+const Video = styled.video`
+  border: 1px solid blue;
+  width: 50%;
+  height: 50%;
+`;
+
+function App() {
+  const [localVideoRef, setLocalVideoRef] = useState("");
+  return (
+    <section>
+      <div>
+        {UserVideo}
+        {PartnerVideo}
+      </div>
+      <div>
+        {Object.keys(users).map((key) => {
+          if (key === yourID) {
+            return null;
+          }
+          return <button onClick={() => callPeer(key)}>Call {key}</button>;
+        })}
+      </div>
+      <div>{incomingCall}</div>
+    </section>
+  );
+}
+
+export default App;
